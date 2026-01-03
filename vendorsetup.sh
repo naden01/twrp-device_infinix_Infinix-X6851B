2cef19ddd27c80512e3c586eac518d3c7004d150
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-01-03 12:55:01 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 git clone https://github.com/ramabondanp/twrp-device_transsion_mt6855-common.git -b fox_12.1 device/transsion/mt6855-common
+
 bash device/transsion/mt6855-common/vendorsetup.sh
+
+export OF_DEFAULT_KEYMASTER_VERSION=4.1
