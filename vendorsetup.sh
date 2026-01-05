020466acb73d9ce735fc3f4d295c373683d03c09
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-01-05 20:05:19 +0700

    im dumb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # common
-git clone https://github.com/RamaBP-Recovery-Project/twrp-device_transsion_mt6855-common.git -b fox_12.1-tranos15 device/transsion/mt6895-common
+git clone https://github.com/RamaBP-Recovery-Project/twrp-device_transsion_mt6855-common.git -b fox_12.1-tranos15 device/transsion/mt6855-common
 
 export FOX_ASH_IS_BASH=1
 export FOX_DELETE_AROMAFM=1
